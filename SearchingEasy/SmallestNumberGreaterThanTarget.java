23c26aa0c53601b13ab9b387b1a9a0172b6c17cb
Pranav <pranavtri00@gmail.com>
2021-12-16 23:55:22 +0530

    Added more solutions

---
+++ SearchingEasy/SmallestNumberGreaterThanTarget.java
@@ -0,0 +1,20 @@
+package com.Leetcode.SearchingEasy;
+
+public class SmallestNumberGreaterThanTarget {
+    public static void main(String[] args) {
+        char[] letters={'c','f','j'};
+        System.out.println(nextGreatestLetter(letters,'j'));
+    }
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int l=0;
+        int r=letters.length-1;
+
+        while(l<r && letters[r]>target){
+            int mid=l+(r-l)/2;
+
+            if(letters[mid]>target) r=mid;
+            else l=mid+1;
+        }
+        return letters[l];
+    }
+}
