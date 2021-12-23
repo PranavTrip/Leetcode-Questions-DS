4a79adfd376e2307edfee67e68f3614a03cdc814
Pranav <pranavtri00@gmail.com>
2021-12-23 21:34:17 +0530

    Added more Solutions

---
+++ LinkedListEasy/LinkedListCycle.java
@@ -0,0 +1,27 @@
+package com.Leetcode.LinkedListEasy;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while (fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+ class ListNode {
+     int val;
+      ListNode next;
+      ListNode(int x) {
+        val = x;
+         next = null;
+     }
+  }
