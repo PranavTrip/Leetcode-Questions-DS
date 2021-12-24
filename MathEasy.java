3706f6b5443dd2b36663e1211c66a2a9e1445b0a
Pranav <pranavtri00@gmail.com>
2021-12-24 22:40:09 +0530

    Added More Solutions

---
+++ MathEasy.java
@@ -0,0 +1,21 @@
+package com.Leetcode;
+
+public class MathEasy {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int reverse = 0;
+        int num = x;
+        while (x > 0) {
+            int temp = x % 10;
+            x /= 10;
+            reverse = reverse * 10 + temp;
+        }
+        return reverse == num;
+    }
+}
