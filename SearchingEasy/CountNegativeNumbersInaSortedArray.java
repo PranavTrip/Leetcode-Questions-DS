1e79de0338d21f66223e5718df1aa5bfa5071c33
Pranav <pranavtri00@gmail.com>
2022-01-09 23:46:22 +0530

    Optimized a solution

--- SearchingEasy/CountNegativeNumbersInaSortedArray.java
+++ SearchingEasy/CountNegativeNumbersInaSortedArray.java
@@ -2,18 +2,17 @@ package com.Leetcode.SearchingEasy;
 
 public class CountNegativeNumbersInaSortedArray {
     public static void main(String[] args) {
-        int [][] grid={{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        int[][] grid = {{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
         System.out.println(countNegatives(grid));
     }
+
     public static int countNegatives(int[][] grid) {
-        int count=0;
-        for(int i=0;i<grid.length;i++){
-            for (int j=0;j<grid[0].length;j++){
-                if(grid[i][j]<0){
-                    count++;
-                }
-            }
+        int n=grid.length,m=grid[0].length, row=0, column=m - 1,ans=0;
+        while (row < n) {
+            while (column >= 0 && grid[row][column] < 0) column--;
+            ans += m - column - 1;
+            row++;
         }
-        return count;
+        return ans;
     }
 }
