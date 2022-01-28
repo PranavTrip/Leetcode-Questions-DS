6ca2d4ea5d838a854c506d97105217d988f77afa
Pranav <pranavtri00@gmail.com>
2022-01-28 09:54:09 +0530

    Added more solutions

---
+++ ArraysEasy/MajorityElement.java
@@ -0,0 +1,21 @@
+package com.Leetcode.ArraysEasy;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,1,1,1};
+        System.out.println(majorityElement(nums));
+    }
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
