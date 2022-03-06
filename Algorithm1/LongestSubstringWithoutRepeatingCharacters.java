bb35777118c22ef2e366bca5ab4cdb0e11a97e97
Pranav <pranavtri00@gmail.com>
2022-03-07 00:15:52 +0530

    Added more solutions

--- Algorithm1/LongestSubstringWithoutRepeatingCharacters.java
+++ Algorithm1/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,10 +1,28 @@
 package com.Leetcode.Algorithm1;
 
+import java.util.HashSet;
+
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
 
     }
-    public static int lengthOfLongestSubstring(String s) {
 
+    public static int lengthOfLongestSubstring(String s) {
+        int traverse = 0;
+        int start = 0;
+        int size = 0;
+
+        HashSet<Character> hs = new HashSet<>();
+        while (traverse < s.length()) {
+            if (!hs.contains(s.charAt(traverse))) {
+                hs.add(s.charAt(traverse));
+                traverse++;
+                size = Math.max(size, hs.size());
+            } else {
+                hs.remove(s.charAt(start));
+                start++;
+            }
+        }
+        return size;
     }
 }
