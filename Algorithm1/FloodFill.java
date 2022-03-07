614d087d7e8912a6aab31f52c9d2573b84646b98
Pranav <pranavtri00@gmail.com>
2022-03-07 16:51:12 +0530

    Added more solutions

---
+++ Algorithm1/FloodFill.java
@@ -0,0 +1,22 @@
+package com.Leetcode.Algorithm1;
+
+public class FloodFill {
+    public static void main(String[] args) {
+
+    }
+    public static void solve(int[][] image, int sr, int sc, int newColor,int cur)
+    {
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[sr].length || image[sr][sc]==newColor || image[sr][sc]!=cur)
+            return;
+        image[sr][sc]=newColor;
+        solve(image,sr+1,sc,newColor,cur);
+        solve(image,sr,sc+1,newColor,cur);
+        solve(image,sr-1,sc,newColor,cur);
+        solve(image,sr,sc-1,newColor,cur);
+    }
+    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor)
+    {
+        solve(image,sr,sc,newColor,image[sr][sc]);
+        return image;
+    }
+}
