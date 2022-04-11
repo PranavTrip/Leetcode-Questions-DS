80033bda69369d38a0f8d232a54385e08e69aadd
Pranav <pranavtri00@gmail.com>
2022-04-11 14:47:57 +0530

    Added more solutions

---
+++ LinkedListEasy/AddTwoNumbers.java
@@ -0,0 +1,34 @@
+package com.Leetcode.LinkedListEasy;
+
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode p = l1;
+        ListNode q = l2;
+        ListNode curr = dummy;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum=x+y+carry;
+            carry=sum/10;
+            curr.next=new ListNode(sum%10);
+            curr=curr.next;
+            if(p!=null){
+                p=p.next;
+            }
+            if(q!=null){
+                q=q.next;
+            }
+        }
+        if(carry>0){
+            curr.next=new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
