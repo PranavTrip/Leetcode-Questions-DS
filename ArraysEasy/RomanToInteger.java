641046d64c84644a48bcfb13fe71f0e1664a14c6
Pranav <pranavtri00@gmail.com>
2022-05-17 23:40:39 +0530

    Added more solutions

---
+++ ArraysEasy/RomanToInteger.java
@@ -0,0 +1,33 @@
+package com.Leetcode.ArraysEasy;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+
+    }
+    public static int romanToInt(String s) {
+        int sum = 0;
+        int a = 0;
+        for(int i = 0;i < s.length(); i++){
+            int b = getValue(s.charAt(i));
+            if(b>a){
+                sum -= (2 * a);
+            }
+            sum += b;
+            a = b;
+        }
+        return sum;
+    }
+
+    public static int getValue(char c){
+        switch(c){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
+        }
+    }
+}
