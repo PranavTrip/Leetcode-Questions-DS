cfb01ec8da582c2a5ebfa26f1de158b7ace8f4fe
Pranav <pranavtri00@gmail.com>
2022-07-14 19:34:37 +0530

    Added more solutions

---
+++ DynamicProgrammingEasy/HouseRobber.java
@@ -0,0 +1,42 @@
+package com.Leetcode.DynamicProgrammingEasy;
+
+import java.util.Arrays;
+
+public class HouseRobber {
+    public static void main(String[] args) {
+        int[] nums={2,7,9,3,1};
+        int n = nums.length;
+        System.out.println(rob(nums,n));
+    }
+    public static int rob(int[] nums,int n) {
+        int[] dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return tabulation(nums,dp,n);
+    }
+    public static int solve(int ind,int[] nums,int[]dp,int n){
+        if(ind==0){
+            return nums[ind];
+        }
+        if(ind<0){
+            return 0;
+        }
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        int pick=solve(ind-2,nums,dp,n) + nums[ind];
+        int notPick= solve(ind - 1, nums,dp,n);
+        return dp[ind]=Math.max(pick,notPick);
+    }
+    public static int tabulation(int[] nums,int[] dp,int n){
+        dp[0]=nums[0];
+        for(int i=1;i< nums.length;i++){
+            int pick=nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int notPick=dp[i-1];
+            dp[i]=Math.max(pick,notPick);
+        }
+        return dp[nums.length-1];
+    }
+}
