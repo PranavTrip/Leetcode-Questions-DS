c148b2ff5b7bea8d390b37f320a615531683e51c
Pranav <pranavtri00@gmail.com>
2022-08-03 15:28:14 +0530

    Added more solutions

---
+++ BinaryTrees/RangeSumOfBST.java
@@ -0,0 +1,19 @@
+package com.Leetcode.BinaryTrees;
+
+public class RangeSumOfBST {
+    public static void main(String[] args) {
+
+    }
+    public int rangeSumBST(TreeNode root, int low, int high) {
+
+        if(root==null){
+            return 0;
+        }
+       if(root.val>=low && root.val<=high){
+           return root.val+rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
+       }
+       return rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
+
+
+    }
+}
